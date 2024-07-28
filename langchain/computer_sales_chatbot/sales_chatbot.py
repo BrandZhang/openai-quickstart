c2cf7b388738271301b55e43c372e21413f9848e
ZHANGBR5 <brand.zhang@oocl.com>
2024-07-28 14:27:13 +0800

    Day 3 - practice computer sales chatbot

--- langchain/computer_sales_chatbot/sales_chatbot.py
+++ langchain/computer_sales_chatbot/sales_chatbot.py
@@ -4,17 +4,18 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate
 import os
 
 
-def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
+def initialize_sales_bot(vector_store_dir: str="real_computer_sales"):
     db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(base_url=os.getenv("BASE_URL")),allow_dangerous_deserialization=True)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,base_url = os.getenv("BASE_URL"))
     
     global SALES_BOT    
     SALES_BOT = RetrievalQA.from_chain_type(llm,
                                            retriever=db.as_retriever(search_type="similarity_score_threshold",
-                                                                     search_kwargs={"score_threshold": 0.8}))
+                                                                     search_kwargs={"score_threshold": 0.6}))
     # 返回向量数据库的检索结果
     SALES_BOT.return_source_documents = True
 
@@ -29,13 +30,22 @@ def sales_chat(message, history):
     ans = SALES_BOT({"query": message})
     # 如果检索出结果，或者开了大模型聊天模式
     # 返回 RetrievalQA combine_documents_chain 整合的结果
-    if ans["source_documents"] or enable_chat:
+    if ans["source_documents"]:
         print(f"[result]{ans['result']}")
         print(f"[source_documents]{ans['source_documents']}")
         return ans["result"]
-    # 否则输出套路话术
+    # 否则输出话术
     else:
-        return "这个问题我要问问领导"
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,base_url = os.getenv("BASE_URL"))
+        chat_input = ChatPromptTemplate.from_messages([
+            ("system",
+             "You are a knowledgeable insurance salesman."
+             "Please Short answer the question as if you are talking to a customer"),
+             ("user","{message}")
+        ])
+        chat_response = chat_input | llm
+
+        return chat_response.invoke(message).content
     
 
 def launch_gradio():
@@ -50,7 +60,7 @@ def launch_gradio():
     demo.launch(share=True, server_name="0.0.0.0")
 
 if __name__ == "__main__":
-    # 初始化房产销售机器人
+    # 初始化电脑销售机器人
     initialize_sales_bot()
     # 启动 Gradio 服务
     launch_gradio()
